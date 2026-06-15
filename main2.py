de72bf608ffefc88cadd617fb7136254c229f226
vannessleung <96851859+vannessleung@users.noreply.github.com>
2026-06-15 22:24:44 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,15 +1,16 @@
 import serpapi
 import json
+import os
 
-client = serpapi.Client(api_key="SERPAPI_KEY")
+client = serpapi.Client(api_key=os.environ["SERPAPI_KEY"])
 results = client.search({
   "engine": "google_flights",
-  "departure_id": "CDG",
-  "arrival_id": "AUS",
-  "currency": "USD",
-  "type": "2",
-  "outbound_date": "2026-06-16"
+  "departure_id": "LHR",
+  "arrival_id": "HKG",
+  "currency": "GBP",
+  "type": "1",
+  "outbound_date": "2026-12-24",
+  "return_date": "2027-01-21"
 })
-best_flights = results["best_flights"]
 
 print(json.dumps(results, indent=2))
