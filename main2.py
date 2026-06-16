95d5fa94c3fc78428e02eb039cdd12386ab92a82
vannessleung <96851859+vannessleung@users.noreply.github.com>
2026-06-16 19:41:39 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -9,8 +9,15 @@ results = client.search({
   "arrival_id": "HKG",
   "currency": "GBP",
   "type": "1",
+  "gl": "uk",
+  "adults": "3",
+  "sort_by": "2",
+  "stops": "2",
+  "layover_duration": 0.360
   "outbound_date": "2026-12-24",
   "return_date": "2027-01-21"
+  
+  
 })
 
 print(json.dumps(dict(results), indent=2))
